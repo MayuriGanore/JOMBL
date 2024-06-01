6a1bf08d9b50226a233fbe030fa72b95f114acea
Mayuri Ganore <mayurig_t21018@students.isquareit.edu.in>
2024-06-01 22:01:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+dependencies installed:
+1)npm install @nestjs/mongoose mongoose bcryptjs
+2)npm install jsonwebtoken
+
+
+To run the Code use:
+npm run start:dev
---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {Model} from "mongoose";
+import * as bcrypt from "bcrypt";
+import * as jwt from 'jsonwebtoken';
+import {User,UserDocument} from "./schemas/user.schemas";
+
+@Injectable()
+export class UsersService {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    constructor(
+        @InjectModel(User.name)
+        private userModel:Model<UserDocument>
+    ){}
+
+    async register(email:string,password:string):Promise<User>
+    {
+        const UserExists=await this.userModel.findOne({email});
+        if(UserExists)
+            throw new Error("Email Address Already In Use");
+
+        const HashedPassword=await bcrypt.hash(password,10);
+        const user=new this.userModel({email,password:HashedPassword});
+        return user.save();
+    }  
+    async login(email:string,password:string):Promise<string>
+    {
+        const user=await this.userModel.findOne({email});
+        if(!email)
+        {
+            throw Error("User Not Found!!");
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid)
+        {
+            throw Error ("Password Not Valid!!");
+        }
+        const token=jwt.sign({email:user.email},'MayuriGanore@24//',{expiresIn:"1h"});
+        return token;
+    }
+}
